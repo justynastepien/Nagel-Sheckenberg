e09c7686ada27a9ee2cabce5432bd0d309b79d1d
justynastepien <jstepie@student.agh.edu.pl>
2022-11-23 20:02:49 +0100

    Two line symulation, acceleration function added

--- Traffic symulation/Model.py
+++ Traffic symulation/Model.py
@@ -6,9 +6,11 @@ import Car
 
 
 FREE_LANE = 201
+MAX_SPEED = 4
 
 white = (255, 255, 255)
 black = (0, 0, 0)
+blue = (0, 0, 255)
 red = (255, 0, 0)
 
 
@@ -24,10 +26,15 @@ def draw_grid(screen, width):
 def draw(board, screen):
     for j in range(board.shape[1]):
         for i in range(board.shape[0]):
-            if board[i][j] == 0:
+            if board[i][j] == 0 and j == 1:
                 pygame.draw.rect(screen, black, (i * 10 + 1, 501, 9, 9))
                 continue
-            pygame.draw.rect(screen, red, (i * 10 + 1, 501, 9, 9))
+            elif j == 1:
+                pygame.draw.rect(screen, blue, (i * 10 + 1, 501, 9, 9))
+            elif board[i][j] == 0 and j == 0:
+                pygame.draw.rect(screen, black, (i * 10 + 1, 491, 9, 9))
+            elif j == 0:
+                pygame.draw.rect(screen, blue, (i * 10 + 1, 491, 9, 9))
 
 
 def find_free_id(board):
@@ -39,13 +46,19 @@ def find_free_id(board):
 
 
 def add_random_car(board, cars, screen):
-    p = np.random.rand(1)
+    p = np.random.rand(2)
 
-    if p > 0.5 and board[0][1] == 0:
+    if p[0] > 0.5 and board[0][1] == 0:
         id = find_free_id(board)
         cars.append(Car.Car(1, id))
-        board[0][1] = 1
-        pygame.draw.rect(screen, red, (0 * 10 + 1, 501, 9, 9))
+        board[0][1] = id
+        pygame.draw.rect(screen, blue, (0 * 10 + 1, 501, 9, 9))
+
+    if p[1] > 0.5 and board[199][0] == 0:
+        id = find_free_id(board)
+        cars.append(Car.Car(1, id))
+        board[199][0] = id
+        pygame.draw.rect(screen, blue, (199 * 10 + 1, 491, 9, 9))
 
 
 def find_car(id, cars):
@@ -55,17 +68,30 @@ def find_car(id, cars):
 
 
 def find_next_car(board, i, j):
-    for k in range(i + 1, board.shape[0]):
-        if not board[k][j] == 0:
-            next_car = board[k][j]
-            return next_car, k
-    return FREE_LANE, FREE_LANE
+    match j:
+        case 0:
+            for k in range(i):
+                if not board[k][j] == 0:
+                    next_car = board[k][j]
+                    return next_car, k
+            return FREE_LANE, FREE_LANE
+        case 1:
+            for k in range(i + 1, board.shape[0]):
+                if not board[k][j] == 0:
+                    next_car = board[k][j]
+                    return next_car, k
+            return FREE_LANE, FREE_LANE
 
+def f_acceleration(velocity):
+    if velocity == 1:
+        return 2
+    else:
+        return 1
 
 def process(board, cars):
 
     new_board = np.zeros((200, 2))
-
+    cars_to_rmv = []
     for j in range(board.shape[1]):
         for i in range(board.shape[0]):
 
@@ -80,14 +106,14 @@ def process(board, cars):
             next_car, k = find_next_car(board, i, j)
 
             if next_car == FREE_LANE:
-                new_vel = vel + 1 #tu ma być funkcja przyrostu
+                new_vel = vel + f_acceleration(vel)
 
             if not next_car == FREE_LANE:
-                distance = k - i - 1
+                distance = abs(k - i) - 1
                 if distance > vel:
-                    new_vel = vel + 1 #tu ma być funkcja przyrostu
+                    new_vel = vel + f_acceleration(vel)
                 elif distance < vel:
-                    new_vel = distance - 1  # tu ma być funkcja hamowania
+                    new_vel = distance - 1  #funkcja hamowania
                     if distance == 1:
                         new_vel = 1
                     elif distance == 0:
@@ -95,7 +121,16 @@ def process(board, cars):
                 else:
                     new_vel = vel
 
+            new_vel = min(new_vel, MAX_SPEED)
             car.change_velocity(new_vel)
-            if i + new_vel < 200:
+            if i + new_vel < 200 and j == 1:
                 new_board[i + new_vel][j] = car.id
-    return new_board
+
+            elif i-new_vel >= 0 and j == 0:
+                new_board[i - new_vel][j] = car.id
+            else:
+                cars_to_rmv.append(car)
+    for c in cars_to_rmv:
+        cars.remove(c)
+
+    return new_board, cars
--- Traffic symulation/main.py
+++ Traffic symulation/main.py
@@ -11,7 +11,7 @@ red = (255, 0, 0)
 
 (width, height) = (1900, 1000)
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Nagel Schreckenberg Symulation")
+pygame.display.set_caption("Nagel-Schreckenberg Symulation")
 updateTime = 0.05
 
 running = True
@@ -22,18 +22,9 @@ cars = []
 
 Model.draw_grid(screen, width)
 
-cars.append(Car.Car(4, 1))
-cars.append(Car.Car(1, 2))
-
-board[10][1] = 1
-board[13][1] = 2
-pygame.draw.rect(screen, red, (10*10 + 1, 501, 9, 9))
-pygame.draw.rect(screen, red, (13*10 + 1, 501, 9, 9))
-pygame.display.flip()
-time.sleep(2)
-
 while running:
-    board = Model.process(board, cars)
+
+    board, cars = Model.process(board, cars)
     Model.add_random_car(board, cars, screen)
     Model.draw(board, screen)
     pygame.display.flip()
