4e542ded775361e8c0231536ed41e0d166d10573
justynastepien <jstepie@student.agh.edu.pl>
2022-12-07 21:22:03 +0100

    Bus

---
+++ Traffic symulation/Bus.py
@@ -0,0 +1,13 @@
+class Bus:
+
+    def __init__(self, velocity, id, vehicle_type):
+        self.v = velocity
+        self.id = id
+        self.vehicle_type = vehicle_type
+        self.counter = 0
+
+    def change_velocity(self, velocity):
+        self.v = velocity
+
+    def change_counter(self, i):
+        self.counter = i
